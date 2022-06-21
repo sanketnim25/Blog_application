0e5209301c3e69cd26aecc79cba05862b9c391d3
sanketnim25 <107452858+sanketnim25@users.noreply.github.com>
2022-06-21 15:06:06 +0530

    added html code for new login, and login
    curd operations on user data still incomplete, had a issue with code and an 500 error, it needs to be resolved before moving ahead.

--- Main_page.py
+++ Main_page.py
@@ -9,8 +9,8 @@ import random
 app=Flask(__name__)
 app.secret_key="zonda"
 
-@app.route('/Home_page')
-def Home_page():  
+@app.route('/home_page')
+def home_page():  
     try:
         with mysql.connector.connect(host="localhost",user="root",password="sanket@123",database="SNS") as connection:
             with connection.cursor() as cursor:
@@ -27,9 +27,38 @@ def Home_page():
     page=render_template("Main_page.html",records=result,username=username,records_login_info=records_login_info)
     return page
 
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method=='GET':
+        page=render_template("login.html")
+        return page
+
+    elif request.method=='POST':
+        username=request.form['uname']
+        password=request.form['psw']
+        return redirect(url_for("home_page"))
+
+@app.route('/new_login',methods = ['POST', 'GET'])
+def new_login():
+    if request.method=='GET':
+        print("**************1")
+        page=render_template("new_login.html")
+        return page
+
+    elif request.method=='POST':
+        print("**************2")
+        username=request.form['uname']
+        mail=request.form['mail']
+        mobile_number=request.form['mobile_number']
+        password=request.form['psw']
+        introduction=request.form['introduction']
+        print("**************3")
+        page=render_template('login.html')
+        return page
+
 @app.route('/')
 def default_page():
-    return redirect(url_for("Home_page"))
+    return redirect(url_for("home_page"))
 
 if __name__=='__main__':
     app.run(host="0.0.0.0",port=50000)
